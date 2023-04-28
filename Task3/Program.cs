c32b00d93f3e56f6dade772b6f0b8383214c7363
Denis <denis-topalov@yandex.ru>
2023-04-29 10:44:46 +1100

    Seminar_2. Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Write("Введите день недели, в виде цифры:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+switch (b)
+{
+    case 7:
+        Console.Write($"{b}->Да");
+        break;
+    case 6:
+        Console.Write($"{b}->Да");
+        break;
+    case 5:
+        Console.Write($"{b}->Нет");
+        break;
+    case 4:
+        Console.Write($"{b}->Нет");
+        break;
+    case 3:
+        Console.Write($"{b}->Нет");
+        break;
+    case 2:
+        Console.Write($"{b}->Нет");
+        break; 
+    case 1:
+        Console.Write($"{b}->Нет");
+        break;             
+}
