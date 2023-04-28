7f61cf4bd3f82dbe9a51d3b8ce1f696e49b1a840
Denis <denis-topalov@yandex.ru>
2023-04-29 10:27:45 +1100

    Seminar_2. Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трёхзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.Write("Error");
+    return;
+}
+if (a >= 1000)
+{
+    Console.Write("Error");
+    return;
+}
+else
+{
+    int b = a / 10;
+    int c = b % 10;
+    Console.Write($"->{c}");
+}
